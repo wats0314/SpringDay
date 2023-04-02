8864e86d7aa1a9e96a32ffa7f2856a83581a1aec
Mitch Watson <wats0314@d.umn.edu>
2023-04-02 18:30:32 -0500

    starting js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
 console.log('hello world')
+
+let answer = document.getElementById("attendance").value
+
+console.log(answer)
+
+function attending() {
+    if (document.getElementById("attendance").value = "Yes") {
+    alert("well see you there!")    
+    }
+    else { alert("not a valid response") }
+    console.log(answer)
+}
